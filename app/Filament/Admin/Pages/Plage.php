f9e475d5caaccf908dbf85746df73f66bbcb0b73
Guerra <etiguerra@gmail.com>
2025-03-22 13:27:06 +0100

    ajustes

--- app/Filament/Admin/Pages/Plage.php
+++ app/Filament/Admin/Pages/Plage.php
@@ -85,7 +85,7 @@ class Plage extends Page implements HasForms
             'Content-Type' => 'application/json',
         ])->post('https://api.gowinston.ai/v2/plagiarism', [
             'text' => $data['content'],
-            'file' => ($data['repository_id'] ? asset($tese->file) : ''),
+            'file' => ($data['repository_id'] ? 'https://ispel.dembi.ao/'.$tese->file : ''),
         ]);
 
         if ($response->successful()) {
